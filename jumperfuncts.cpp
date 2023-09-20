2c6831537b6ef5648f64c73dbf9bcea7173ba7f2
CorinneJones6 <jonescorinne6@gmail.com>
2023-09-20 09:31:33 -0600

    updated CMAKE

---
+++ jumperfuncts.cpp
@@ -0,0 +1,8 @@
+//
+//  jumperfuncts.cpp
+//  testSFML
+//
+//  Created by Corinne Jones on 9/18/23.
+//
+
+#include "jumperfuncts.hpp"
---
+++ jumperfuncts.hpp
@@ -0,0 +1,13 @@
+//
+//  jumperfuncts.hpp
+//  testSFML
+//
+//  Created by Corinne Jones on 9/18/23.
+//
+
+#ifndef jumperfuncts_hpp
+#define jumperfuncts_hpp
+
+#include <stdio.h>
+
+#endif /* jumperfuncts_hpp */
