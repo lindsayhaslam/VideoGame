b2466f09668022093edf44c68aa9943fa89cba72
CorinneJones6 <jonescorinne6@gmail.com>
2023-09-21 15:44:20 -0600

    added test files

---
+++ src/TestFuncts.cpp
@@ -0,0 +1,8 @@
+//
+//  TestFuncts.cpp
+//  testSFML
+//
+//  Created by Corinne Jones on 9/21/23.
+//
+
+#include "TestFuncts.hpp"
---
+++ src/TestFuncts.hpp
@@ -0,0 +1,13 @@
+//
+//  TestFuncts.hpp
+//  testSFML
+//
+//  Created by Corinne Jones on 9/21/23.
+//
+
+#ifndef TestFuncts_hpp
+#define TestFuncts_hpp
+
+#include <stdio.h>
+
+#endif /* TestFuncts_hpp */
